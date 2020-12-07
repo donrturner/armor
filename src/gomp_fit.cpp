9d5dda535409e2ebbe17eb825763ecee85570657
Donald Turner <mightbedon@tamu.edu>
2020-12-07 14:19:18 -0600

    Add more comments to gomp_fit_c function and update documentation

--- src/gomp_fit.cpp
+++ src/gomp_fit.cpp
@@ -14,12 +14,12 @@ arma::colvec df_db(const arma::colvec& x, double eta, double b){
     return(-eta * (-exp(eta * (-exp(b * x)) + b * x + eta)) % (b * x % (eta * exp(b * x) - 1) - 1));
 }
 
-// Gompertz distribution function
+// Gompertz density function
 arma::colvec gompfun(const arma::colvec& x, const arma::colvec& par){
     return(par(1) * par(0) * exp(par(0) + par(1) * x - par(0) * exp(par(1) * x)));
 }
 
-// Gompertz model fit using nonlinear least squares (Levenberg-Marquardt algorithm)
+// Gompertz distribution fit using nonlinear least squares (Levenberg-Marquardt algorithm)
 // x - a column vector of ages of length n
 // y - a column vector of length n whose elements are the number of deaths for a given age
 // par - eta and b, the shape and scale parameters for the Gompertz distribution
@@ -36,6 +36,12 @@ Rcpp::List gomp_fit_c(const arma::colvec& x, const arma::colvec& y, const arma::
     arma::colvec lamvec(2);
     obj(1) = std::inner_product(res.begin(), res.end(), res.begin(), 0.0);
     obj(0) = 2 * obj(1);
+
+    // Update is determined by the equation (J^TJ - lambda * I) %*% d = J^T %*% res,
+    // where J is our Jacobian matrix. C is our cost factor. A new step is accepted
+    // if the corresponding value of C is lower than the value of C calculated using
+    // the old parameters
+
     arma::mat J, JTJ, lammat, g;
     arma::colvec dC, newpar, newres;
     double C, Cnew;
@@ -54,12 +60,14 @@ Rcpp::List gomp_fit_c(const arma::colvec& x, const arma::colvec& y, const arma::
         newres = y - gompfun(x, newpar);
         Cnew = 0.5 * std::inner_product(newres.begin(), newres.end(), newres.begin(), 0.0);
 
+        // If our new C is lower, we accept the update and increase step size.
         if(Cnew < C){
             par = newpar;
             res = newres;
             obj(0) = obj(1);
             obj(1) = std::inner_product(res.begin(), res.end(), res.begin(), 0.0);
             lambda /= lamdown;
+        // If our new C is not lower, we do not accept the update and decrease the step size
         }else{
             lambda *= lamup;
         }
