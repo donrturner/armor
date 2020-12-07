1adcbdc5d85a5416de83816321ccf45c4e3c58c4
Donald Turner <mightbedon@tamu.edu>
2020-12-06 23:29:28 -0600

    Fix errors in gomp_fit_c function and update documentation

--- src/gomp_fit.cpp
+++ src/gomp_fit.cpp
@@ -6,17 +6,17 @@
 
 // Calculate the partial derivative of f with respect to eta
 arma::colvec df_deta(const arma::colvec& x, double eta, double b){
-    return(b * exp(eta * (-exp(b * x)) + b * x + eta) * (eta * (exp(b * x) - 1) - 1));
+    return(b * exp(eta * (-exp(b * x)) + b * x + eta) % (eta * (exp(b * x) - 1) - 1));
 }
 
 // Calculate the partial derivative of f with respect to b
 arma::colvec df_db(const arma::colvec& x, double eta, double b){
-    return(-eta * (-exp(eta * (-exp(b * x)) + b * x + eta)) * (b * x * (eta * exp(b * x) - 1) - 1));
+    return(-eta * (-exp(eta * (-exp(b * x)) + b * x + eta)) % (b * x % (eta * exp(b * x) - 1) - 1));
 }
 
 // Gompertz distribution function
 arma::colvec gompfun(const arma::colvec& x, const arma::colvec& par){
-    return(par(2) * par(1) * exp(par(1) + par(2) * x - par(1) * exp(par(2) * x)));
+    return(par(1) * par(0) * exp(par(0) + par(1) * x - par(0) * exp(par(1) * x)));
 }
 
 // Gompertz model fit using nonlinear least squares (Levenberg-Marquardt algorithm)
@@ -34,19 +34,19 @@ Rcpp::List gomp_fit_c(const arma::colvec& x, const arma::colvec& y, const arma::
     arma::colvec obj(2, arma::fill::zeros);
     arma::colvec res = y - gompfun(x, par);
     arma::colvec lamvec(2);
-    obj(2) = std::inner_product(res.begin(), res.end(), res.begin(), 0.0);
-    obj(1) = 2 * obj(2);
+    obj(1) = std::inner_product(res.begin(), res.end(), res.begin(), 0.0);
+    obj(0) = 2 * obj(1);
     arma::mat J, JTJ, lammat, g;
     arma::colvec dC, newpar, newres;
     double C, Cnew;
 
-    while(obj(1) - obj(2) > eps){
-        J = arma::join_rows(df_deta(x, par(1), par(2)), df_db(x, par(1), par(2)));
-        JTJ = arma::cross(J, J);
+    while(obj(0) - obj(1) > eps){
+        J = arma::join_rows(df_deta(x, par(0), par(1)), df_db(x, par(0), par(1)));
+        JTJ = J.t() * J;
         lamvec.fill(lambda);
         lammat = arma::diagmat(lamvec);
         g = JTJ + lammat;
-        dC = cross(J, res);
+        dC = J.t() * res;
         C = 0.5 * std::inner_product(res.begin(), res.end(), res.begin(), 0.0);
 
         // Evaluate new parameters and cost
@@ -57,8 +57,8 @@ Rcpp::List gomp_fit_c(const arma::colvec& x, const arma::colvec& y, const arma::
         if(Cnew < C){
             par = newpar;
             res = newres;
-            obj(1) = obj(2);
-            obj(2) = std::inner_product(res.begin(), res.end(), res.begin(), 0.0);
+            obj(0) = obj(1);
+            obj(1) = std::inner_product(res.begin(), res.end(), res.begin(), 0.0);
             lambda /= lamdown;
         }else{
             lambda *= lamup;
@@ -66,5 +66,5 @@ Rcpp::List gomp_fit_c(const arma::colvec& x, const arma::colvec& y, const arma::
 
     }
     return Rcpp::List::create(Rcpp::Named("par") = par,
-                              Rcpp::Named("MSE") = obj(2));
+                              Rcpp::Named("MSE") = obj(1));
 }
